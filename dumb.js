5a90cd0fa7370365f2c92c636d3d7061d39a42c8
Nicholas Blott <blottn@tcd.ie>
2024-12-26 16:41:21 +0000

    Remove unused file

--- dumb.js
+++
@@ -1,7 +0,0 @@
-import { flat_prefs } from './preferences.js';
-
-// Relies on all tokens having a preference & no 2 tokens having the same name
-export function dumbProxy(script, available) {
-  let flatscript = script.map(({name, reminders}) => [name, ...reminders]).flat()
-  console.log(flatscript, available);
-}
