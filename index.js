78218ceb90ea588d171daac74c060a268ff7b05b
Nicholas Blott <blottn@tcd.ie>
2025-01-28 15:08:20 +0100

    Added some debug delay

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ import { build } from './lib/visualize.js';
 import script from './data/charming_idiots.v6.json' with {type: 'json'};
 
 let root = document.getElementById("root");
-build(root, script);
+let { save, load } = build(root, script);
+const debug = () => {
+  console.log(save());
+  setTimeout(debug, 1000);
+};
+
+const wait = (time) => new Promise((resolve) => {
+    setTimeout(resolve, time)
+});
+
+await wait("3000")
+
+let conf = {picked: {"villageidiot": ["farmer", "imp", "imp"]}, unavailable: {}}
+load(conf);
+debug();
