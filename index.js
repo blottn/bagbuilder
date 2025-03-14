02027d81e3edfa9d93b8f194c68716481dc15d0c
Nicholas Blott <blottn@tcd.ie>
2025-03-14 15:10:38 +0100

    Add initial mutation listener

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ import controls from './lib/controls.js';
 import script from './data/charming_idiots.v6.json' with {type: 'json'};
 import prefs from './data/preferences.json' with {type: 'json'};
 
-let pickerRoot = document.querySelector(".picker");
-let { done, save, load } = picker(pickerRoot, loadScript(), prefs);
-
 let controlsRoot = document.querySelector(".controls");
+let pickerRoot = document.querySelector(".picker");
+
+let { done, save, load } = picker(pickerRoot, controlsRoot, loadScript(), prefs);
 let ctrl = controls(controlsRoot, pickerRoot);
+
 
 const wait = (time) => new Promise((resolve) => {
     setTimeout(resolve, time)
